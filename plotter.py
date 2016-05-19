b5441e3da854d83c838f4c222109dd66616c401c
Piotr Sliwa <piotr.a.sliwa@gmail.com>
2016-05-19 22:44:09 +0200

    Fix docstrings to agree with Google guidelines

--- plotter.py
+++ plotter.py
@@ -1,14 +1,40 @@
+"""Plotter creates the matrix that is later displayed."""
+
 class Plotter(object):
+    """Creates dotmatrix.
+
+        Attributes:
+            dotmatrix (list of lists of ints): representing
+                dotplot matrix for two analysed sequences.
+    """
 
     def __init__(self, *args, **kwargs):
+        """Inits Plotter with empty dotmatrix."""
         self.dotmatrix = []
 
     def plot(self, sequences):
-        """
-        Creates dotplot matrix for given sequences
+        """Creates dotplot matrix for given sequences.
 
-        Parameters: (str, str) - sequences to analyse
-        Return: list of lists of ints - dotplot matrix for sequences
+        Args:
+            sequences - (str, str): tuple of strings
+                representing sequences to analyse.
+
+        Returns:
+            A list of lists of ints (matrix-like)
+                representing dotplot matrix for the sequences:
+                1 in places where corresponding letters agree,
+                0 in places where corresponding letters do not agree.
+
+            For example:
+               sequences = ("ABA", "ABC")
+
+                   A   B   C
+                A  1   0   0
+                B  0   1   0
+                A  1   0   0
+
+                [[1, 0, 0], [0, 1, 0], [1, 0, 0]] is returned
+
         """
         seq1, seq2 = sequences
 
@@ -20,4 +46,3 @@ class Plotter(object):
                 else:
                     self.dotmatrix[row_index].append(0)
         return self.dotmatrix
-
