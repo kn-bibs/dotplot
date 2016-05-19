6380bd8eac93c35360b29c1c05e045b7b2bf3ab0
Piotr Sliwa <piotr.a.sliwa@gmail.com>
2016-05-19 20:23:43 +0200

    Add basic creation of dot matrix

--- plotter.py
+++ plotter.py
@@ -1,4 +1,23 @@
 class Plotter(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        self.dotmatrix = []
+
+    def plot(self, sequences):
+        """
+        Creates dotplot matrix for given sequences
+
+        Parameters: (str, str) - sequences to analyse
+        Return: list of lists of ints - dotplot matrix for sequences
+        """
+        seq1, seq2 = sequences
+
+        for row_index, vertical_letter in enumerate(seq1):
+            self.dotmatrix.append([])
+            for horizontal_letter in seq2:
+                if horizontal_letter == vertical_letter:
+                    self.dotmatrix[row_index].append(1)
+                else:
+                    self.dotmatrix[row_index].append(0)
+        return self.dotmatrix
+
