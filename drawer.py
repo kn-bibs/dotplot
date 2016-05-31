3b425078074d92bd7dee1da98f0dbc19f326d668
Bartek Henkiel <bartek.henkiel@gmail.com>
2016-05-31 21:20:21 +0200

    wstawilem prosty konstruktor z opcja wyciagniecia z niego macierzy, ustawienia nowej i kompletnie poki co goloa funkcja rysujacja

--- drawer.py
+++ drawer.py
@@ -1,3 +1,28 @@
 class Drawer:
-    def __init__(self):
+
+    def __init__(self, dot_matrix=[]):
+
+        self.dot_matrix = dot_matrix
         pass
+
+    def get_matrix(self):
+
+        return self.dot_matrix
+
+    def set_matrix(self, new_matrix):
+
+        self.dot_matrix = new_matrix
+
+    def draw(self):
+        drawing = ""
+
+        for row in self.dot_matrix:
+
+            for element in row:
+
+                if element == 1:
+                    drawing = drawing + "."
+                else:
+                    drawing = drawing + " "
+
+        return drawing
