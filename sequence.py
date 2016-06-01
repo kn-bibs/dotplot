e743ac438a3a852bff8372568780b49a6e0cb90e
maciosz <a.macioszek@students.mimuw.edu.pl>
2016-06-01 14:38:02 +0200

    Add read_fasta() method

--- sequence.py
+++ sequence.py
@@ -1,3 +1,25 @@
 class Sequence(object):
-    def __init__(self):
-        pass
+
+    def __init__(self, filename):
+        self.name = ''
+        self.sequence = ''
+        self.read_fasta(filename)
+
+    def read_fasta(self, filename):
+        fastafile = open(filename, 'r')
+        self.read_name(fastafile)
+        self.read_sequence(fastafile)
+
+    def read_sequence(self, fastafile):
+        self.sequence = ""
+        for line in fastafile:
+            self.sequence += line.strip()
+
+    def read_name(self, fastafile):
+        name = fastafile.next()
+        if not name.startswith('>'):
+            raise TypeError("Not a FASTA file")
+        name = name.lstrip('>')
+        name = name.rstrip()
+        self.name = name
+
