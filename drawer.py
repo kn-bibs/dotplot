a73e78b51784214e89f45098c709492bb9bde947
Bartek Henkiel <bartek.henkiel@gmail.com>
2016-06-02 18:26:56 +0200

    Add docstrings in Drawer class

--- drawer.py
+++ drawer.py
@@ -1,11 +1,16 @@
 class Drawer(object):
+    """Creates a Drawer object"""
 
     def __init__(self):
         pass
 
 
     def draw(self, dot_matrix):
+        """Prints an ASCII representation of dotmatrix
 
+        Args:
+            dot_matrix - (list): list of lists representing a dotplot matrix
+        """
         for row in dot_matrix:
             for element in row:
                 if element == 1:
