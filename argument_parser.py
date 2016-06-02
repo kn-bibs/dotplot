30cd0935aea6fa5590026cb3ca200502cf0e5c6f
Behoston <mlegiecki@gmail.com>
2016-06-02 18:18:02 +0200

    ArgumentParser now using "namespaces"

--- argument_parser.py
+++ argument_parser.py
@@ -3,6 +3,7 @@
 Parses arguments
 """
 import argparse
+from collections import namedtuple
 
 
 class ArgumentParser(object):
@@ -12,14 +13,20 @@ class ArgumentParser(object):
 
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument('file1', type=argparse.FileType(),
-                                 help='First input file in FASTA format')
-        self.parser.add_argument('file2', type=argparse.FileType(),
-                                 help='Second input file in FASTA format')
+        sequences = self.parser.add_argument_group('sequences')
+        sequences.add_argument('file1', type=argparse.FileType(),
+                               help='First input file in FASTA format')
+        sequences.add_argument('file2', type=argparse.FileType(),
+                               help='Second input file in FASTA format')
 
     def parse(self, arguments):
         """
         Parse given arguments, skips first argument (assume that is script name)
         :type arguments: list
         """
-        return vars(self.parser.parse_args(arguments[1:]))
+        args = self.parser.parse_args(arguments[1:])
+        # fixme: temporary workaround
+        namedtuple1 = namedtuple(
+            'Arguments', ['sequences', 'plotter', 'drawer'])
+        namedtuple1.sequences = [args.file1, args.file2]
+        return namedtuple1
