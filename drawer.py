e032ccabacdbd6f042ec5b02c8d2935db0ffabc2
Bartek Henkiel <bartek.henkiel@gmail.com>
2016-06-02 16:43:38 +0200

    "poprawki estetyczne w pliku drawer.py wzgledem poprzedniej wersji"

--- drawer.py
+++ drawer.py
@@ -1,26 +1,20 @@
-class Drawer:
-
+class Drawer(object):
+    
     def __init__(self, dot_matrix=[]):
-
         self.dot_matrix = dot_matrix
         pass
 
     def get_matrix(self):
-
         return self.dot_matrix
 
     def set_matrix(self, new_matrix):
-
         self.dot_matrix = new_matrix
 
     def draw(self):
-
         drawing = ""
 
         for row in self.dot_matrix:
-
             for element in row:
-
                 if element == 1:
                     drawing = drawing + "x"
                 else:
