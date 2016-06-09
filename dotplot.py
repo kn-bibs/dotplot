1715a1310ea68e5cf963ea921f5b5cc43f044de6
Behoston <mlegiecki@gmail.com>
2016-06-09 16:45:53 +0200

    added missing namespaces and fix dotplot to read file1 and file2 instead list of files

--- dotplot.py
+++ dotplot.py
@@ -6,9 +6,9 @@ from sequence import Sequence
 
 
 class Dotplot(object):
-
-    def __init__(self,  sequences, plotter_args=None, drawer_args=None):
-        self.sequences = [Sequence.from_fasta_file(seq) for seq in sequences]
+    def __init__(self, sequences, plotter_args=None, drawer_args=None):
+        self.sequences = [Sequence.from_fasta_file(sequences.file1),
+                          Sequence.from_fasta_file(sequences.file2)]
         self.plotter = Plotter(plotter_args)
         self.drawer = Drawer(drawer_args)
 
@@ -19,6 +19,7 @@ class Dotplot(object):
         if not plot:
             plot = self.plot
         self.drawer.draw(plot)
+
 
 if __name__ == '__main__':
     args = ArgumentParser().parse(sys.argv)
