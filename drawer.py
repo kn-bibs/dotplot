b06e4859aad334b58660fc9ff94bd3d4916d2f17
pjanek <pj344973@students.mimuw.edu.pl>
2016-06-29 22:21:06 +0200

    Improve and fix draw_unicode method in Drawer
    Unicode version of plot looks more, more fancy now, so it's
    unicode by default from now.

--- drawer.py
+++ drawer.py
@@ -1,15 +1,15 @@
 class Drawer(object):
     """Creates a Drawer object"""
 
-    def __init__(self, arguments=None):
-        """Creates a Drawer object that is using a given drawing method
-
-        TODO: write an accurate docstring
-        """
-        if arguments.mode is None:
-            self.draw = self.draw_ascii
+    def __init__(self, arguments):
+        """Creates a Drawer object that is using a given drawing method"""
+        #TODO: write an accurate docstring
+        #TODO: exceptions
+        if not hasattr(arguments, 'mode'):
+            self.draw = self.draw_unicode
         else:
-            drawing_methods = {'unicode' : self.draw_unicode, 'ascii' : self.draw_ascii}
+            drawing_methods = {'unicode' : self.draw_unicode,
+                               'ascii' : self.draw_ascii}
             self.draw = drawing_methods[arguments.mode]
 
     def draw_ascii(self, dot_matrix):
@@ -21,9 +21,9 @@ class Drawer(object):
         for row in dot_matrix:
             for element in row:
                 if element == 1:
-                    print('X', end='')
+                    print('\x20', end='')
                 else:
-                    print(' ', end='')
+                    print('\x58', end='')
             print('')
 
     def draw_unicode(self, dot_matrix):
@@ -32,26 +32,17 @@ class Drawer(object):
         Args:
             dot_matrix - (list): list of lists representing a dotplot matrix
         """
-        #left upper corner
-        print(u'\u2554', end='')
-        #upper line
-        for i in range(len(dot_matrix[0])):
-            print(u'\u2550', end='')
-        #right upper corner
-        print(u'\u2557', end='\n')
-        #matrix
-        for row in dot_matrix:
+        print(u'\u2554', end='')                    #left upper corner
+        print(u'\u2550'*len(dot_matrix[0]), end='') #upper line
+        print(u'\u2557')                            #right upper corner
+        for row in dot_matrix:                      #matrix...
             print(u'\u2551', end='')
             for element in row:
                 if element == 1:
-                    print('X', end='')
+                    print(u'\u2588', end='')
                 else:
-                    print(' ', end='')
+                    print(u'\u0020', end='')
             print(u'\u2551')
-        #left lower corner
-        print(u'\u255A', end='')
-        #lower line
-        for i in range(len(dot_matrix[0])):
-            print(u'\u2550', end='')
-        #right lower corner
-        print(u'\u255D', end='\n')
+        print(u'\u255A', end='')                    #left lower corner
+        print(u'\u2550'*len(dot_matrix[0]), end='') #lower line
+        print(u'\u255D')                            #right lower corner
