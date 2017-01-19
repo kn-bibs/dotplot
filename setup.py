d5af4ed6c619aa43c0675db0f1e7f87891f5be5c
krassowski <krassowski.michal@gmail.com>
2017-01-19 23:40:25 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import find_packages
 setup(
     name='dotplot',
     packages=find_packages(),
-    version='0.3',
+    version='0.4',
     license='LGPL-3.0',
     description='Small bioinformatic package for dotplot\'s generation (in command line and in GUI)',
     author='kn_bibs',
     author_email='bibs.kn@uw.edu.pl',
     url='https://github.com/kn-bibs/dotplot',
-    download_url='https://github.com/kn-bibs/dotplot/tarball/v0.3-alpha',
+    download_url='https://github.com/kn-bibs/dotplot/tarball/v0.4-alpha',
     keywords=['dotplot', 'bioinformatic', 'gui'],
     classifiers=[
         'Development Status :: 3 - Alpha',
