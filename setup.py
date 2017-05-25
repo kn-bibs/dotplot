91f7e4e32d087fb083b4e1d062b79737460f2911
krassowski <krassowski.michal@gmail.com>
2017-05-26 00:47:59 +0200

    Add manifest, bump version

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ except ImportError:
 setup(
     name='dotplot',
     packages=find_packages(),
-    version='0.4.4',
+    version='0.4.5',
     license='LGPL-3.0',
     description='Small bioinformatic package for dotplot\'s generation (in command line and in GUI)',
     long_description=get_long_description('README.md'),
     author='kn_bibs',
     author_email='bibs.kn@uw.edu.pl',
     url='https://github.com/kn-bibs/dotplot',
-    download_url='https://github.com/kn-bibs/dotplot/tarball/v0.4.4-alpha',
+    download_url='https://github.com/kn-bibs/dotplot/tarball/v0.4.5-alpha',
     keywords=['dotplot', 'bioinformatic', 'gui'],
     classifiers=[
         'Development Status :: 3 - Alpha',
