eb78e2b51584db637c0783732a62ce072c534309
krassowski <krassowski.michal@gmail.com>
2017-05-26 14:14:09 +0200

    Add basic download/uniprot parsing tests

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -1,6 +1,9 @@
+from collections import namedtuple
 from tempfile import NamedTemporaryFile
+import requests_mock
 from pytest import raises
 from dotplot.sequence import Sequence
+from dotplot.sequence import DownloadFailed
 
 
 def create_named_temp_file(data=''):
@@ -25,7 +28,44 @@ def test_load():
 
         assert sequence.name == 'some_name'
         assert sequence.sequence == 'ACTGTACG'
+        assert len(sequence) == 8
 
     with open(create_named_temp_file(invalid_fasta)) as invalid_sequence_file:
         with raises(TypeError):
             Sequence.from_fasta_file(invalid_sequence_file)
+
+
+def test_download():
+
+    test_datum = namedtuple('TestData', 'sequence_id url response sequence_name sequence constructor')
+
+    test_data = [
+        test_datum(
+            'P03086',
+            'http://www.uniprot.org/uniprot/P03086.fasta',
+            (
+                '>sp|P03086|AGNO_POVJC Agnoprotein OS=JC polyomavirus PE=1 SV=1\n'
+                'MVLRQLSRKASVKVSKTWSGTKKRAQRILIFLLEFLLDFCTGEDSVDGKKRQRHSGLTEQ\n'
+                'TYSALPEPKAT'
+            ),
+            'sp|P03086|AGNO_POVJC Agnoprotein OS=JC polyomavirus PE=1 SV=1',
+            'MVLRQLSRKASVKVSKTWSGTKKRAQRILIFLLEFLLDFCTGEDSVDGKKRQRHSGLTEQTYSALPEPKAT',
+            Sequence.from_uniprot
+        )
+    ]
+
+    with requests_mock.mock(real_http=False) as mocked_requests:
+        for data in test_data:
+            mocked_requests.register_uri('GET', data.url, text=data.response)
+            sequence = Sequence.from_uniprot(data.sequence_id)
+            assert sequence.name == data.sequence_name
+            assert sequence.sequence == data.sequence
+
+        # as real_http is set to false, all requests other than registered will return 404
+        mocked_requests.register_uri(
+            'GET',
+            'http://www.uniprot.org/uniprot/P00000.fasta',
+            status_code=404
+        )
+        with raises(DownloadFailed):
+            Sequence.from_uniprot('P00000')
